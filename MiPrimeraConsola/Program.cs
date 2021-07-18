1438c6a8fe0a8538a1fec0057b25e28bc92d749f
Jeniffer171096 <71280031+Jeniffer171096@users.noreply.github.com>
2021-07-17 20:05:45 -0500

    Add files via upload

---
+++ MiPrimeraConsola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MiPrimeraConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                Console.WriteLine($"Hay{args.Length} argumentos");
+                Console.WriteLine($"Tu nombre es:  {args[0] }");
+                if (args.Length > 1)
+                    Console.WriteLine($"Tu apellido es: {args[1] }");
+            }
+            else
+                Console.WriteLine("No hay Argumentos");
+        }
+
+
+
+
+    }
+    
+}
